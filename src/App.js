6157840ce7df9e7f1d29acc308b716dbc5adb9ab
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2022-10-12 23:36:27 +0900

    2022/10/12 study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState, useEffect } from "react";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [keyword, setKeyword] = useState("");
+
+//   const onClick = () => setCounter((prev) => prev + 1);
+//   const onChange = (event) => setKeyword(event.target.value);
+
+//   // console.log("i run all the time");
+//   useEffect(() => {
+//     console.log("I run only once.");
+//   }, []);
+
+//   useEffect(() => {
+//     console.log("I run when 'keywrod' changes.");
+//   }, [keyword]);
+
+//   useEffect(() => {
+//     console.log("I run when 'counter' changes.");
+//   }, [counter]);
+
+//   useEffect(() => {
+//     console.log("I run when keyword & counter changes.");
+//   }, [counter, keyword]);
+//   return (
+//     <div>
+//       <input
+//         value={keyword}
+//         onChange={onChange}
+//         type="text"
+//         placeholder="Search here..."
+//       />
+//       <h1>{counter}</h1>
+//       <button onClick={onClick}>click me</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  });
+  // useEffect(function () {
+  //   console.log("hi :)");
+  //   return function () {
+  //     console.log("bye :(");
+  //   };
+  // }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
